340aaef34261997599f1c5181597003617f8f860
Marc Fournier <marc.fournier@camptocamp.com>
2011-11-15 13:05:20 +0100

    file renaming to be compliant with recommended module structure.

--- manifests/classes/duplicity.pp
+++
@@ -1,6 +0,0 @@
-class duplicity {
-  case $operatingsystem {
-    Debian, Ubuntu: { include duplicity::debian }
-    default: { fail "$operatingsystem not yet implemented for $name" }
-  }
-}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,6 @@
-import "classes/*.pp"
-import "definitions/*.pp"
+class duplicity {
+  case $operatingsystem {
+    Debian, Ubuntu: { include duplicity::debian }
+    default: { fail "$operatingsystem not yet implemented for $name" }
+  }
+}
